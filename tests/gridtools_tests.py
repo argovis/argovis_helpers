cccd5b1bac5bac9dffe47ee4161f5ed76e92f6ae
billmills <you@example.com>
2023-10-26 12:51:43 -0400

    adding gridtools module

--- tests/gridtools_tests.py
+++ tests/gridtools_tests.py
@@ -9,28 +9,27 @@ class TestClass():
         return
 
     def test_label_features_basic(self):
-    	# binary_features = [
-    	# 	[0,0,0,0,0,0,0,0],
-    	# 	[0,1,1,0,0,0,0,0],
-    	# 	[0,1,1,0,0,0,0,0],
-    	# 	[0,0,0,1,0,0,0,0],
-    	# 	[0,0,0,0,0,0,0,0],
-    	# 	[0,0,0,0,0,1,1,0],
-    	# 	[0,0,0,0,0,1,0,0],
-    	# 	[0,0,0,0,0,0,0,0]
-    	# ]
+    	binary_features = [
+    		[0,0,0,0,0,0,0,0],
+    		[0,1,1,0,0,0,0,0],
+    		[0,1,1,0,0,0,0,0],
+    		[0,0,0,1,0,0,0,0],
+    		[0,0,0,0,0,0,0,0],
+    		[0,0,0,0,0,1,1,0],
+    		[0,0,0,0,0,1,0,0],
+    		[0,0,0,0,0,0,0,0]
+    	]
 
-    	# correct_labels - [
-    	# 	[0,0,0,0,0,0,0,0],
-    	# 	[0,1,1,0,0,0,0,0],
-    	# 	[0,1,1,0,0,0,0,0],
-    	# 	[0,0,0,1,0,0,0,0],
-    	# 	[0,0,0,0,0,0,0,0],
-    	# 	[0,0,0,0,0,2,2,0],
-    	# 	[0,0,0,0,0,2,0,0],
-    	# 	[0,0,0,0,0,0,0,0]
-    	# ]
+    	correct_labels = numpy.array([
+    		[0,0,0,0,0,0,0,0],
+    		[0,1,1,0,0,0,0,0],
+    		[0,1,1,0,0,0,0,0],
+    		[0,0,0,1,0,0,0,0],
+    		[0,0,0,0,0,0,0,0],
+    		[0,0,0,0,0,2,2,0],
+    		[0,0,0,0,0,2,0,0],
+    		[0,0,0,0,0,0,0,0]
+    	])
 
-    	# labeled_map = gridtools.label_features(binary_features)
-    	#numpy.testing.assert_array_equal(correct_labels, labeled_map)
-    	assert True
+    	labeled_map = gridtools.label_features(binary_features)
+    	assert correct_labels.tolist() == labeled_map.tolist()
