7fcff63742b07609f3af3ad8a555afaac279b8eb
billmills <you@example.com>
2023-10-27 00:08:28 -0400

    unit test identifying failing edge case

--- tests/gridtools_tests.py
+++ tests/gridtools_tests.py
@@ -2,6 +2,7 @@ from argovisHelpers import gridtools
 import numpy, pytest
 
 def test_label_features_basic():
+	# simple check for two distinct regions
 	binary_features = [
 		[0,0,0,0,0,0,0,0],
 		[0,1,1,0,0,0,0,0],
@@ -27,3 +28,164 @@ def test_label_features_basic():
 	labeled_map = gridtools.label_features(binary_features)
 	assert correct_labels.tolist() == labeled_map.tolist()
 
+def test_label_features_first_pole():
+	# check pole features are getting grouped by default
+	binary_features = [
+		[0,1,1,0,0,1,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	]
+
+	correct_labels = numpy.array([
+		[0,1,1,0,0,1,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	])
+
+	labeled_map = gridtools.label_features(binary_features)
+	assert correct_labels.tolist() == labeled_map.tolist()
+
+def test_label_features_first_pole_no_connection():
+	# check pole features are not grouped when grouping turned off
+	binary_features = [
+		[0,1,1,0,0,1,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	]
+
+	correct_labels = numpy.array([
+		[0,1,1,0,0,2,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	])
+
+	labeled_map = gridtools.label_features(binary_features, connected_poles=False)
+	assert correct_labels.tolist() == labeled_map.tolist()
+
+def test_label_features_last_pole():
+	# check pole features are getting grouped by default
+	binary_features = [
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,1,0,0,0,1,0,0]
+	]
+
+	correct_labels = numpy.array([
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,1,0,0,0,1,0,0]
+	])
+
+	labeled_map = gridtools.label_features(binary_features)
+	assert correct_labels.tolist() == labeled_map.tolist()
+
+def test_label_features_last_pole_no_connection():
+	# check pole features are not grouped when grouping turned off
+	binary_features = [
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,1,0,0,0,1,0,0]
+	]
+
+	correct_labels = numpy.array([
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,1,0,0,0,0,0,0],
+		[0,1,0,0,0,2,0,0]
+	])
+
+	labeled_map = gridtools.label_features(binary_features, connected_poles=False)
+	assert correct_labels.tolist() == labeled_map.tolist()
+
+def test_label_features_dateline():
+	# check dateline features are getting grouped by default
+	binary_features = [
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[1,1,0,0,0,0,0,0],
+		[1,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,1],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	]
+
+	correct_labels = numpy.array([
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[1,1,0,0,0,0,0,0],
+		[1,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,1],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	])
+
+	labeled_map = gridtools.label_features(binary_features)
+	assert correct_labels.tolist() == labeled_map.tolist()
+
+def test_label_features_dateline_no_connection():
+	# check dateline features are not grouped when grouping turned off
+	binary_features = [
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[1,1,0,0,0,0,0,0],
+		[1,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,1],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	]
+
+	correct_labels = numpy.array([
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[1,1,0,0,0,0,0,0],
+		[1,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,2],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0]
+	])
+
+	labeled_map = gridtools.label_features(binary_features, periodic_dateline=False)
+	assert correct_labels.tolist() == labeled_map.tolist()
